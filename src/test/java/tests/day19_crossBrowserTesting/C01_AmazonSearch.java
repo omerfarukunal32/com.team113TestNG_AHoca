50fc2ec703362ebb34e130df91abae1ae0572a0c
Ömer Ünal <mrfrknl3232@gmail.com>
2023-05-25 21:23:43 +0200

    25.05/V2

---
+++ src/test/java/tests/day19_crossBrowserTesting/C01_AmazonSearch.java
@@ -0,0 +1,39 @@
+package tests.day19_crossBrowserTesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.Driver;
+import utilities.TestBaseCross;
+
+public class C01_AmazonSearch extends TestBaseCross {
+
+    @Test
+    public void amazonTest(){
+
+        // amazon ana sayfaya gidelim
+        driver.get("https://www.amazon.com");
+
+        // Nutella icin arama yapalim
+        // Arama kutusunun locate'i pages sayfasinda
+        // POM'de pages sayfasindaki locate'lere erismek icin
+        // pages sayfalarindan bir obje olusturulur.
+
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+
+
+        // sonuclarin Nutella icerdigini test edelim
+        String expecredIcerik ="Nutella";
+        WebElement aramaSonucuElementi = driver.findElement(By.xpath("//*[@class='a-color-state a-text-bold']"));
+        String actualAramaSonucuElementi = aramaSonucuElementi.getText();
+        Assert.assertTrue(actualAramaSonucuElementi.contains(expecredIcerik));
+
+        // sayfayi kapatalim
+        // parent Class'da teardown oldugu icin, sayfayi otomatik kapatacak.
+
+    }
+}
